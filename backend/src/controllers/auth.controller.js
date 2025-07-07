cb59e1100814c48b015e6a6cbcad282fa36737d0
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-07-07 13:48:20 -0400

    feat: backend setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => {
+    // Handle user signup logic here
+    res.send("Signup Page");
+}
+
+export const login = (req, res) => {
+    // Handle user login logic here
+    res.send("Login Page");
+}
+
+export const logout = (req, res) => {
+    // Handle user logout logic here
+    res.send("Logout Page");
+}
